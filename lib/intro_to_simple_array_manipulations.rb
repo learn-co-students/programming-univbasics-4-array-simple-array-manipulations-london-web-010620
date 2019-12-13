116c75af0d9e1054e9b0e312cc1ef090f3b36971
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-13 20:45:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(arr,color)
+  arr.push(color)
+end
+
+def using_unshift(arr,bourough)
+  arr.unshift(bourough)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr, things)
+  arr.concat(things)
+end
+
+def using_insert(arr, language)
+  arr.insert(4,language)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, instructors)
+  arr.delete(instructors)
+end
+
+def using_delete_at(arr,integer)
+  arr.delete_at(integer)
+end
