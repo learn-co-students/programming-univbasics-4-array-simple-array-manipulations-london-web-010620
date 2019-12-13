2d05f69f4902be8f2db7921bafcd43ac895b808d
Simon Tucker <sjtucker28@gmail.com>
2019-12-13 00:23:43 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,8 +46,8 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array, string)
-  array.delete(string)
+def using_delete(a, b)
+  a.delete(b)
 end
 
 def using_delete_at(array, integer)
