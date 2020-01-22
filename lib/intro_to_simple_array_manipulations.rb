415bba375701a13f53cca7b695753d0edc86e2f2
shola abereoran <sholaabe@hotmail.co.uk>
2020-01-22 11:22:08 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+def using_push(array, string)
+    array.push(string)
+end
+
+def using_unshift(array, string)
+    array.unshift(string)
+end
+
+def using_pop(array)
+    a = array.pop
+    return a
+end
+
+def pop_with_args(array)
+    a = array.pop(2)
+    return a
+end
+
+def using_shift(array)
+    a = array.shift()
+    return a
+end
+
+def shift_with_args(array)
+    a = array.shift(2)
+    return a
+end 
+
+def using_concat(array1, array2)
+    array1.concat(array2)
+end
+
+def using_insert(array, element)
+    array.insert(4, element)
+end 
+
+def using_uniq(array)
+    array.uniq 
+end
+
+def using_flatten(array)
+    array.flatten 
+end
+
+def using_delete(array, string)
+    array.delete(string)
+
+end
+
+def using_delete_at(array, integer)
+    array.delete_at(integer)
+end
